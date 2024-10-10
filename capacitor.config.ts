9555c1d0f80d39b253ce5d363062fa4d9f4ed409
ty-mxm <mammoliti.ty@gmail.com>
2024-10-10 09:41:45 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myApp02',
+  webDir: 'dist'
+};
+
+export default config;

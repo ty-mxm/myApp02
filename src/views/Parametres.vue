799e9173d7344579320a49802e993b0dfd438b2f
sofiakrins <sofiaisabelkrins@gmail.com>
2024-10-13 20:21:50 -0400

    Update styles and layout in Parametres.vue page

--- src/views/Parametres.vue
+++ src/views/Parametres.vue
@@ -1,30 +1,38 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
+    <ion-header class="header">
+      <ion-toolbar color="primary">
         <ion-title>Paramètres</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content class="ion-padding">
-      <ion-list>
-        <ion-item>
-          <ion-label>Prénom</ion-label>
-          <ion-input v-model="utilisateur.prenom" placeholder="Prénom"></ion-input>
-        </ion-item>
-        <ion-item>
-          <ion-label>Nom</ion-label>
-          <ion-input v-model="utilisateur.nom" placeholder="Nom"></ion-input>
-        </ion-item>
-        <ion-item>
-          <ion-label>E-mail</ion-label>
-          <ion-input v-model="utilisateur.email" placeholder="E-mail" readonly></ion-input>
-        </ion-item>
-      </ion-list>
+    <ion-content class="settings-content">
+      <div class="settings-container">
+        <ion-list>
+          <ion-item>
+            <ion-label>Prénom</ion-label>
+            <ion-input v-model="utilisateur.prenom" placeholder="Prénom"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label>Nom</ion-label>
+            <ion-input v-model="utilisateur.nom" placeholder="Nom"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-label>E-mail</ion-label>
+            <ion-input v-model="utilisateur.email" placeholder="E-mail" readonly></ion-input>
+          </ion-item>
+        </ion-list>
 
-      <ion-button expand="block" @click="changerMotDePasse">Changer mot de passe</ion-button>
-      <ion-button expand="block" @click="deconnexion">Déconnecter</ion-button>
+        <ion-button expand="block" @click="changerMotDePasse">Changer mot de passe</ion-button>
+        <ion-button expand="block" @click="deconnexion">Déconnecter</ion-button>
+      </div>
     </ion-content>
+
+    <ion-footer class="footer">
+      <ion-toolbar color="secondary">
+        <ion-title>Ty Mammoliti et Sofia Krins 2024 ™</ion-title>
+      </ion-toolbar>
+    </ion-footer>
   </ion-page>
 </template>
 
@@ -81,5 +89,28 @@ export default defineComponent({
 </script>
 
 <style scoped>
-/* Ajoutez des styles personnalisés si nécessaire */
+.settings-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+  /* Conteneur stylé pour les paramètres */
+.settings-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 400px;
+  margin: 80px auto 0;
+  padding: 20px;
+  border: 2px solid #f48fb1; /* Bordure rose */
+  border-radius: 10px;
+  background-color: rgba(206, 147, 216, 0.8); /* Arrière-plan violet lavande */
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1); /* Ombre pour un effet de profondeur */
+}
+
+ion-list {
+  margin-bottom: 20px;
+}
 </style>
